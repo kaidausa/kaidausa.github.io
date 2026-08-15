b541c9b6aa68d5b060a53a627985eaaeb0618923
kaidausa <kaida.usa.new@gmail.com>
2026-08-15 01:31:58 -0400

    Update API_URL to use the live Google Apps Script

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.KAIDA_CONFIG = {
-  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbww0NR8gctdiXvnmj6MPc2RGiFRxv4DXpJyFHYsorO7s2vpz2ms0qVtJBy7ox0j-oa3NA/exec",
   SITE_URL: "https://kaidausa.github.io"
 };
