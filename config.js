d2594940f65ea8ccdcafe38260a9d14f87781e8c
kaidausa <kaida.usa.new@gmail.com>
2026-08-15 00:26:34 -0400

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.KAIDA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbym2eKX6vCjQNXmvgEFnC3X0VrG_kK_qEDbdzFeocfxB8lOJbemqyTABzrUvytE6iFL/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxdJGfaBV6dJpfxMyBrb0GaSl-mmWk11fwo_0s404DIskmZ3vuIliG4DkO8fUzXQQ7f/exec",
   SITE_URL: "https://kaidausa.github.io",
   ORGANIZATION: "Kailahun Descendants Association USA",
   REGISTRATION_FEE: 50,
