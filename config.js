6cc5514dbcbbae4a22c4592827b2b37f812ac453
kaidausa <kaida.usa.new@gmail.com>
2026-08-15 02:13:14 -0400

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.KAIDA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxsyPMdYlssz8rWB3wrSZNSkN97ApBuQzcc8yx8cYRWRzUY_RpYFWrJE_z8hCoU6AGmSA/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbw3xhGjpDaMFsjOsk59T9YMKqBClclfeQ8qgxrN0R0cRmOwuVcJiVLf2ZX8aKsS50n27A/exec",
   SITE_URL: "https://kaidausa.github.io"
 };
